5cec3c93d1ee25b7ce8769c57e563685d2e31c8f
gitdela <scriptingdela@gmail.com>
2022-11-14 19:29:38 +0000

    Add a function to change size dynamically

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
 
-for (let i = 0; i < 256; i++) {
+function populateBoard(size) {
+
     const easDrawSpace = document.querySelector('.eas-draw-space');
-    let square = document.createElement('div');
-    // let squares = document.querySelectorAll('div');
-    easDrawSpace.style.gridTemplateColumns = 'repeat(16, 1fr)';
-    // easDrawSpace.style.gridTemplateRows = 'repeat(16, 1fr)';
-    easDrawSpace.appendChild(square);
-    square.addEventListener('mouseover', colorSquare);
+    easDrawSpace.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+
+    const squares = easDrawSpace.querySelectorAll('div');
+    squares.forEach((square) => square.remove());
+
+    let amount = size * size;
+
+    for (let i = 0; i < amount; i++) {
+        let square = document.createElement('div');
+        // let squares = document.querySelectorAll('div');
+        // easDrawSpace.style.gridTemplateRows = 'repeat(16, 1fr)';
+        easDrawSpace.appendChild(square);
+        square.addEventListener('mouseover', colorSquare);
+    }   
 }
 
 function colorSquare() {
-    this.style.backgroundColor = 'black';
+        this.style.backgroundColor = 'black';
+}
+
+populateBoard(16); 
+
+function changeSize(newSize) {
+    populateBoard(newSize);
 }
